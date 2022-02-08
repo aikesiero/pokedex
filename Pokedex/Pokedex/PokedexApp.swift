7d9bf7712ffcc30d4b6cf86d8f0f42b6e5a3197a
Aike Fernández Roza <aikesiero@gmail.com>
2022-02-08 20:23:42 +0100

    Initial Commit

---
+++ Pokedex/Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Aike Fernández Roza on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
